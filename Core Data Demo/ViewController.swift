3e06201557966b8fc50031390f6271ee4a4ab35d
Daniel J Janiak <danieljjaniak@icloud.com>
2016-07-14 15:56:21 -0700

    Create entities and attributes, test by saving and retrieving sample data

--- Core Data Demo/ViewController.swift
+++ Core Data Demo/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /* Set up access to the data store */
+        let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedObjectContext: NSManagedObjectContext = appDelegate.managedObjectContext
+        
+        /* Insert a new user into the data store */
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: managedObjectContext)
+        // Set values for the attributes
+        newUser.setValue("Daniel", forKey: "username")
+        newUser.setValue("dadada", forKey: "password")
+        
+        do {
+            try managedObjectContext.save()
+        } catch {
+            fatalError("Unable to save data")
+        }
+        
+        /* Retrieve info from the data store */
+        let fetchRequest = NSFetchRequest(entityName: "Users")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try managedObjectContext.executeFetchRequest(fetchRequest)
+            print(results)
+            
+            if results.count > 0 {
+                for item in results as! [NSManagedObject] {
+                    print(item.valueForKey("username")!)
+                    print(item.valueForKey("password")!)
+                }
+            }
+            
+        } catch {
+            fatalError("Failed to retrieve data from the data store ")
+        }
+        
+        
+        
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
